9bf2805ae533e9a0ee2f06be255b3f0dd37336de
Mohammed-Alhilaly <mohammedalhilalyai@gmail.com>
2025-03-07 16:34:23 +0300

    Define user roles
     Created UserRole enum for admin and student roles.
     Implemented Roles decorator to assign roles to routes.
     Added RolesGuard to enforce role-based access control.
     Registered RolesGuard globally in AppModule.
     Added JwtAuthGuard for JWT authentication."

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { PrismaModule } from 'prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { RolesGuard } from './auth/auth.role.guard';
 import { UsersModule } from './users/users.module';
-import { PrismaModule } from 'prisma/prisma.module';
 
 @Module({
   imports: [
@@ -14,6 +16,9 @@ import { PrismaModule } from 'prisma/prisma.module';
     PrismaModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService , {
+    provide: APP_GUARD,
+    useClass: RolesGuard,
+  }],
 })
 export class AppModule {}
