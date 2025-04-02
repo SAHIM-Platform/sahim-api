cb248f55fb28bc03f2414d6e7230c0c761518b7c
muneeb-almoliky <muneebalmoliky@gmail.com>
2025-04-03 02:43:27 +0300

    feat(auth): add approved student guard with admin bypass - Implemented ApprovedStudentGuard to restrict access to approved students only. - Allowed admins to bypass the guard. - Added HTTP test file for verifying the guard behavior.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApprovedStudentGuard } from '@/auth/guards/approved-student.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('test-approved-student')
+  @UseGuards(ApprovedStudentGuard)
+  testApprovedStudent(@Req() req) {
+    return { message: `You are an approved student or an admin ${req.user.sub}!` };
+  }
 }
