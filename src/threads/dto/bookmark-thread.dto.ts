7206f474417baf50b3623f11075431b192302b43
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-04-04 11:19:14 +0300

    Revert "feat(threads): add endpoints for bookmarking, unbookmarking threads, and retrieving user bookmarks"

--- src/threads/dto/bookmark-thread.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsInt, Min } from 'class-validator';
-
-export class BookmarkThreadDto {
-  @IsInt()
-  @Min(1, { message: 'Thread ID must be a positive integer' })
-  thread_id: number;
-}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApprovedStudentGuard } from '@/auth/guards/approved-student.guard';
-import { GetUser } from '@/auth/decorators/get-user.decorator';
 
 @Controller('users')
 export class UsersController {
@@ -11,10 +10,5 @@ export class UsersController {
   @UseGuards(ApprovedStudentGuard)
   testApprovedStudent(@Req() req) {
     return { message: `You are an approved student or an admin ${req.user.sub}!` };
-  }
-
-  @Get('me/bookmarks')
-  getUserBookmarks(@GetUser('sub') userId) {
-    return this.usersService.getUserBookmarks(userId);
   }
 }
