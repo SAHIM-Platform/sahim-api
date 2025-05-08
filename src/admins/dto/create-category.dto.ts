1fed96645a6f9d54c596fa5b2943239aec02a94d
muneeb-almoliky <muneebalmoliky@gmail.com>
2025-05-08 23:37:38 +0300

    fix(categories): sanitize and validate names to ensure clean and frontend-safe input

--- src/admins/dto/create-category.dto.ts
+++ src/admins/dto/create-category.dto.ts
@@ -1,16 +1,26 @@
-import { IsString, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+import { IsString, IsNotEmpty, MaxLength, MinLength, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import * as sanitizeHtml from 'sanitize-html';
 
 export class CreateCategoryDto {
   @ApiProperty({
     description: 'Category name',
     example: 'Programming',
     minLength: 2,
-    maxLength: 50
+    maxLength: 50,
+    pattern: '^(?=.*[A-Za-z\u0600-\u06FF])[A-Za-z\u0600-\u06FF ]+$'
   })
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(2, { message: 'Category name must be at least 3 characters long' })
-  @MaxLength(50, { message: 'Category name cannot be longer than 50 characters' })
+  @IsString({ message: 'Category name must be a string.' })
+  @IsNotEmpty({ message: 'Category name cannot be empty.' })
+  @MinLength(2, { message: 'Category name must be at least $constraint1 characters long.' })
+  @MaxLength(50, { message: 'Category name cannot be longer than $constraint1 characters.' })
+  @Matches(/^(?=.*[A-Za-z\u0600-\u06FF])[A-Za-z\u0600-\u06FF ]+$/, {
+    message:
+      'Category name must contain at least one letter (English or Arabic) and only letters and spaces.',
+  })
+  @Transform(({ value }) =>
+    sanitizeHtml(value, { allowedTags: [], allowedAttributes: {} }).trim()
+  )
   name: string;
 }
